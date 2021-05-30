7166b0d39e454270f20ac04fc326cf976ad769f8
Thibault Albatro <35143389+talbatro@users.noreply.github.com>
2021-05-30 20:06:41 +0200

    Add files via upload

---
+++ LAZYCHF.py
@@ -0,0 +1,15 @@
+# Code Chef Practice (Beginner) Problem
+# Lazy Chef - Find the amount of work lazy Chef does
+
+T = int(input()) # test cases input
+
+for t in range(T): # iterate over each test case
+
+    x, m, d = map(int, input().split()) # map each input to ints
+
+    lazy_work = x * m # total lazy work
+    delay_work = x + d # maximum delayed work
+    
+    actual_work = min(x*m, x + d) # actual work is the smallest between the two
+
+    print(actual_work) # print out result
