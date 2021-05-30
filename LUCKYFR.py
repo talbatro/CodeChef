617861fe17b0c3106dfb2db8eb6e8ca71d36ff4a
Thibault Albatro <35143389+talbatro@users.noreply.github.com>
2021-05-30 19:57:18 +0200

    Update LUCKYFR.py

--- LUCKYFR.py
+++ LUCKYFR.py
@@ -6,7 +6,6 @@ T = int(input()) # test cases input
 for t in range(T): # iterate over each test case
     
     number = list(input()) # transform integer input into list
-    N = len(number) # find size of list
         
     cnt = 0 # initialize counter
     for digit in number: # iterate over each digit of number
