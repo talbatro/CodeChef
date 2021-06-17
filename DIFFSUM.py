64d6f5e3a414056764a5b0c2081b329693aecad0
Thibault Albatro <35143389+talbatro@users.noreply.github.com>
2021-06-17 18:56:45 +0200

    Add files via upload

--- DIFFSUM.py
+++ DIFFSUM.py
@@ -1,10 +1,10 @@
 # Code Chef Practice (Beginner) Problem
 # Sum OR Difference - Find sum or difference if 1st number is greater
 
-N1 = int(input())
-N2 = int(input())
+N1 = int(input()) # input 1st number
+N2 = int(input()) # input 2nd number
 
-if N1 > N2:
-    print(N1 - N2)
+if N1 > N2: # check if 1st is greater than 2nd
+    print(N1 - N2) # print out difference
 else:
-    print(N1 + N2)
+    print(N1 + N2) # otherwise print out sum
