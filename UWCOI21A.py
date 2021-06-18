878f35be5a2f276063544b947fa6fa381b44ed95
Thibault Albatro <35143389+talbatro@users.noreply.github.com>
2021-06-18 12:49:42 +0200

    Add files via upload

---
+++ UWCOI21A.py
@@ -0,0 +1,11 @@
+# Code Chef Practice (Beginner) Problem
+# Hidden Numbers
+
+T = int(input()) # input test cases amount
+
+for t in range(T): # iterate over each test case
+
+    N = int(input()) # input number as integer
+
+    print(1, N) # Simplest multiplication is 1 * N
+    
